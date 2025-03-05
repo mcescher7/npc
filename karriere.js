53e2c9803f7c92bb0c494a9e4f5be8d2d3d807aa
mcescher7 <mcescher@live.de>
2025-03-05 13:47:55 +0100

    Update karriere.js

--- karriere.js
+++ karriere.js
@@ -4,6 +4,19 @@ document.addEventListener("DOMContentLoaded", async function() {
     const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
     const searchInput = document.getElementById('search-player');
 
+    // Funktion zum Formatieren des Datums
+    function formatDate(date) {
+        const rowTime = new Date(date);
+        return rowTime.toLocaleString('de-DE', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false, // Verhindert das 12-Stunden-Format
+        }) + ' Uhr';
+    }
+
     async function loadCareerData(player = '') {
         const { data, error } = await supabaseClient
             .from("roster_changes")
@@ -18,14 +31,7 @@ document.addEventListener("DOMContentLoaded", async function() {
         const tableBody = document.getElementById("roster-changes-table");
         tableBody.innerHTML = data.map(row =>
             `<tr>
-                <td>${row.time.toLocaleString('de-DE', {
-                        day: '2-digit',
-                        month: '2-digit',
-                        year: 'numeric',
-                        hour: '2-digit',
-                        minute: '2-digit',
-                        hour12: false,
-                    }) + ' Uhr'}</td>
+                <td>${formatDate(row.time)}</td>
                 <td>${row.type}</td>
                 <td>${row.manager_name}</td>
                 <td>${row.target}</td>
@@ -33,7 +39,7 @@ document.addEventListener("DOMContentLoaded", async function() {
         ).join("");       
     }
 
-     searchInput.addEventListener('input', function() {
+    searchInput.addEventListener('input', function() {
         const player = searchInput.value.trim().toLowerCase();
         loadCareerData(player);
     });
