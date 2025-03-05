56d496939b252743efafb6dad0a404ccfa2d48cf
mcescher7 <mcescher@live.de>
2025-03-05 09:52:54 +0100

    Update rekorde.js

--- rekorde.js
+++ rekorde.js
@@ -39,11 +39,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.querySelectorAll(".filter-btn").forEach(button => {
         button.addEventListener("click", () => {
-            document.querySelectorAll(".filter-btn").forEach(btn => btn.classList.remove("btn-primary"));
-            button.classList.add("btn-primary");
+            document.querySelectorAll(".filter-btn").forEach(btn => btn.classList.remove("active")); 
+            button.classList.add("active");
+            
             loadTopPerformances(button.getAttribute("data-position"));
         });
     });
 
+    
     loadTopPerformances();
 });
